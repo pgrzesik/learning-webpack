908686c8a3e5a255bc6b6ae236680ed78976a35a
Piotr Grzesik <pj.grzesik@gmail.com>
2017-07-19 00:24:40 +0200

    Add examples of config for loaders

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,31 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.join(__dirname, "./build")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.jpeg$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 10000
+            }
+          }
+        ]
+      }
+    ]
   }
 }
